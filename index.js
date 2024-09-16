959930e2b45693a2895dcb4befe5c7e4b86237e8
Lukas.J.Han <lukas.j.han@gmail.com>
2024-09-16 09:58:51 +0900

    security: patch rce and xss

--- index.js
+++ index.js
@@ -2,9 +2,20 @@ const express = require('express');
 const sqlite3 = require('sqlite3').verbose();
 const bodyParser = require('body-parser');
 const path = require('path');
+const helmet = require('helmet');
 
 const app = express();
 const db = new sqlite3.Database(':memory:');
+
+app.use(helmet());
+app.use(
+  helmet.contentSecurityPolicy({
+    directives: {
+      defaultSrc: ["'self'"],
+      scriptSrc: ["'self'", "'unsafe-inline'"],
+    },
+  })
+);
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
@@ -25,18 +36,23 @@ db.serialize(() => {
 
 app.get('/', (req, res) => {
   db.all('SELECT * FROM users', (err, users) => {
+    if (err) {
+      console.error(err);
+      return res.status(500).send('An error occurred');
+    }
     res.render('index', { users: users });
   });
 });
 
 app.post('/add-user', (req, res) => {
-  const { username, email } = req.query;
+  const { username, email } = req.body;
   db.run(
     'INSERT INTO users (username, email) VALUES (?, ?)',
     [username, email],
     (err) => {
       if (err) {
         console.error(err);
+        return res.status(500).send('An error occurred');
       }
       res.redirect('/');
     }
@@ -50,10 +66,15 @@ app.get('/user/:id', (req, res) => {
       console.error(err);
       return res.status(500).send('An error occurred');
     }
-    res.render('user', { user: user, query: req.query });
+    res.render('user', { user: user, message: req.query.message });
   });
 });
 
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('Something broke!');
+});
+
 app.listen(8080, () => {
-  console.log('Server running on http://localhost:8080');
+  console.log('Server running on <http://localhost:8080>');
 });
